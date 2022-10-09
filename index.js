b86b2dcd6f54abe25f51dbc1223259d5d25974cf
Luna Seifert <thepuddingdeity@gmail.com>
2022-10-08 18:01:24 -0600

    commit tree 47701e56f3acd511e37e52e7b2afa406c5548fa8

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+  const arrayOfString = word.split('')
+  let stringReversed = arrayOfString[arrayOfString.length - 1]
+  debugger
+  for (let i = arrayOfString.length - 2; i > -1; i--){
+    stringReversed += arrayOfString[i]
+    debugger
+  }
 
+  if (word === stringReversed){
+    return true
+  }
+  else{
+    console.log(stringReversed)
+    return false
+  }
+
+
+}
+isPalindrome("racecar")
 /* 
   Add your pseudocode here
+  split string into array
+  for loop counting down by the length of the array{
+    take last element of array, and push it into a new string until fully populated
+  }
+  check if new string matches old string
+  return true/false
 */
 
 /*
